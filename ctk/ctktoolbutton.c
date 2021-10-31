5b2da10b8e5d4937507d67ad1942f770c78d3f47
Pablo Barciela <scow@riseup.net>
2021-10-31 02:20:54 +0100

    test

--- ctk/ctktoolbutton.c
+++ ctk/ctktoolbutton.c
@@ -395,8 +395,8 @@ ctk_tool_button_init (CtkToolButton      *button)/*,
 
   /* create button */
 //  button->priv->button = g_object_new (klass->button_type, NULL);
-//  button->priv->button = g_object_new (CTK_TOOL_BUTTON_CLASS (button)->button_type, NULL);
-  button->priv->button = g_object_new ((CtkToolButtonClass *) (button)->button_type, NULL);
+  button->priv->button = g_object_new (CTK_TOOL_BUTTON_CLASS (button)->button_type, CTK_TYPE_BUTTON);
+//  button->priv->button = g_object_new ((CtkToolButtonClass *) (button)->button_type, NULL);
   ctk_widget_set_focus_on_click (CTK_WIDGET (button->priv->button), FALSE);
   g_signal_connect_object (button->priv->button, "clicked",
 			   G_CALLBACK (button_clicked), button, 0);
