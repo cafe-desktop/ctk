e8c6d3ed0cd9e29107a2566bc5cc54934d388dac
Pablo Barciela <scow@riseup.net>
2021-10-31 11:10:53 +0100

    test

--- ctk/ctktoolbutton.c
+++ ctk/ctktoolbutton.c
@@ -360,8 +360,8 @@ ctk_tool_button_class_init (CtkToolButtonClass *klass)
 }
 
 static void
-ctk_tool_button_init (CtkToolButton      *button,
-		      CtkToolButtonClass *klass)
+ctk_tool_button_init (CtkToolButton      *button)//,
+//		      CtkToolButtonClass *klass)
 {
 /*      const GInterfaceInfo actionable_info =
       {
@@ -397,7 +397,7 @@ ctk_tool_button_init (CtkToolButton      *button,
   button->priv->button = g_object_new (klass->button_type, NULL);
 //  button->priv->button = g_object_new (CTK_TOOL_BUTTON_CLASS (button)->button_type, NULL);
 //  button->priv->button = g_object_new (G_OBJECT_CLASS (ctk_tool_button_parent_class)->button_type, NULL);
-//  button->priv->button = g_object_new ((CtkToolButtonClass *) (button)->button_type, NULL);
+  button->priv->button = g_object_new (((CtkToolButtonClass *) (button))->button_type, NULL);
   ctk_widget_set_focus_on_click (CTK_WIDGET (button->priv->button), FALSE);
   g_signal_connect_object (button->priv->button, "clicked",
 			   G_CALLBACK (button_clicked), button, 0);
